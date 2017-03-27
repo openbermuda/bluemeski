f3fb9d6a041414b023f4b28b4b02307c006fea30
Johnny Gill <swfiua@gmail.com>
2017-03-27 11:35:03 -0300

    convert gulcalc output to python lists

--- bluemeski/ktwo.py
+++ bluemeski/ktwo.py
@@ -9,4 +9,25 @@ import struct
 
 def gul2py(infile):
     """ Generatori to process a ktools gul stream """
-    pass
+
+    fmt = 'i'
+    fsize = struct.caclsize(fmt)
+    
+    stream_type = struct.unpack_from(fmt, infile.read(fsize))
+    samplesize = struct.unpack_from(fmt, infile.read(fsize))
+
+    hfmt = '2i'
+    hsize = stuct.calcsize(hfmt)
+    event, item = struct.unpack_from(hfmt, infile.read(hsize))
+
+    rfmt = 'if'
+    rsize = struct.calcsize(hfmt)
+
+    while True:
+        rec = infile.read(rsize)
+
+        if len(rec != rsize):
+            break
+
+        yield struct.unpack_from(rfmt, rec)
+    
