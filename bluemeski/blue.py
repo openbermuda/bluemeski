0e6f1a75923b67bc522d96477c67df76a55b2ec5
Johnny Gill <swfiua@gmail.com>
2017-03-30 17:51:21 -0300

    add --path option to blume show a bunch of csv's

--- bluemeski/blue.py
+++ bluemeski/blue.py
@@ -6,14 +6,29 @@ Watch the stream go by
 
 """
 
-from karmapi import pigfarm
+from karmapi import pigfarm, base
 
 class Magic(pigfarm.MagicCarpet):
     pass
-
+    
 def main():
 
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--path', default=None)
+
+    args = parser.parse_args()
+
     farm = pigfarm.PigFarm()
+
+    if args.path:
+
+        data = base.load_folder(args.path)
+
+        farm.data.put(data)
+    
 
     from karmapi.mclock2 import GuidoClock
 
