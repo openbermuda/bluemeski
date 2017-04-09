db9e2edb138d3d2552126985f1b68cffb0a145ce
Johnny Gill <swfiua@gmail.com>
2017-04-09 20:56:24 +0000

    change blume interface --today option too

--- bluemeski/blue.py
+++ bluemeski/blue.py
@@ -5,6 +5,8 @@ Watch the stream go by
 
 
 """
+from datetime import datetime
+from pathlib import Path
 
 from karmapi import pigfarm, base
 
@@ -17,15 +19,24 @@ def main():
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--path', default=None)
+    parser.add_argument('--path', default='.')
+    parser.add_argument('--today', action='store_true')
+    parser.add_argument('name', nargs='?', default='sensehat')
 
     args = parser.parse_args()
 
     farm = pigfarm.PigFarm()
 
-    if args.path:
+    path = Path(args.path)
 
-        data = base.load_folder(args.path)
+    path = path / args.name
+
+    if args.today:
+        now = datetime.now()
+        path = path / f'{now.year}/{now.month}/{now.day}'
+
+    if path.exists():
+        data = base.load_folder(path)
 
         farm.data.put(data)
     
